1695efef740a614f881b2e1e65c0c1ab8e0b0e8e
mitshell <michau.benoit@gmail.com>
2017-10-22 13:57:24 +0200

    tools: remove painful char

--- tools/pycrate_berdecode.py
+++ tools/pycrate_berdecode.py
@@ -5,7 +5,7 @@
 # * Software Name : pycrate
 # * Version : 0.2
 # *
-# * Copyright © 2017. Benoit Michau. ANSSI.
+# * Copyright 2017. Benoit Michau. ANSSI.
 # *
 # * This program is free software: you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License version 2 as published
