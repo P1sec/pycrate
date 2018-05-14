e34c088fb484cad6966b725e402073e2e04f76d6
mitshell <michau.benoit@gmail.com>
2018-05-14 22:37:28 +0200

    asn1c: fix parsing OPEN value

--- pycrate_asn1c/asnobj.py
+++ pycrate_asn1c/asnobj.py
@@ -5,20 +5,20 @@
 # *
 # * Copyright 2016. Benoit Michau. ANSSI.
 # *
-# * This library is free software; you can redistribute it and/or
-# * modify it under the terms of the GNU Lesser General Public
-# * License as published by the Free Software Foundation; either
-# * version 2.1 of the License, or (at your option) any later version.
-# *
-# * This library is distributed in the hope that it will be useful,
+# * This program is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU General Public License
+# * as published by the Free Software Foundation; either version 2
+# * of the License, or (at your option) any later version.
+# * 
+# * This program is distributed in the hope that it will be useful,
 # * but WITHOUT ANY WARRANTY; without even the implied warranty of
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# * Lesser General Public License for more details.
-# *
-# * You should have received a copy of the GNU Lesser General Public
-# * License along with this library; if not, write to the Free Software
-# * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
-# * MA 02110-1301  USA
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# * 
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+# * 02110-1301, USA.
 # *
 # *--------------------------------------------------------
 # * File Name : pycrate_asn1c/asnobj.py
@@ -5416,9 +5416,13 @@ class ASN1Obj(object):
         colon_offset = search_top_lvl_sep(text, ':')
         if len(colon_offset) == 0:
             return self._parse_value_ref(text)
-        elif len(colon_offset) > 1:
-            raise(ASN1ProcTextErr('{0}: invalid OPEN value, {1}'\
-                  .format(self.fullname(), text)))
+        # 
+        # WNG: in case the OPEN type is a CHOICE, we can have several colons, 
+        # this checks is too strong, disabling it
+        #elif len(colon_offset) > 1:
+        #    raise(ASN1ProcTextErr('{0}: invalid OPEN value, {1}'\
+        #          .format(self.fullname(), text)))
+        #
         textobj, textval = text[:colon_offset[0]].strip(), \
                            text[1+colon_offset[0]:].strip()
         #
