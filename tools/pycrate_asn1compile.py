e91820e9041c91f2a62f6a1eace2dcfee932f22e
mich <benoit.michau@p1sec.com>
2020-02-05 10:34:36 +0100

    tools: update asn1compile with an option to recompile any spec from the asn1dir subdir

--- tools/pycrate_asn1compile.py
+++ tools/pycrate_asn1compile.py
@@ -6,6 +6,7 @@
 # * Version : 0.4
 # *
 # * Copyright 2017. Benoit Michau. ANSSI.
+# * Copyright 2020. Benoit Michau. P1Sec.
 # *
 # * This program is free software: you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License version 2 as published
@@ -32,8 +33,11 @@ import os
 import sys
 import argparse
 
-from pycrate_asn1c.asnproc import compile_text, compile_spec, compile_all, \
-     generate_modules, PycrateGenerator, JSONDepGraphGenerator, ASN_SPECS
+from pycrate_asn1c.asnproc import (
+    compile_text, compile_spec, compile_all, \
+    generate_modules, PycrateGenerator, JSONDepGraphGenerator,
+    ASN_SPECS, GLOBAL, get_spec_dir
+    )
 
 
 # inputs:
@@ -86,8 +90,8 @@ def main():
     #
     parser.add_argument('-i', dest='input', type=str, nargs='+',
                         help='ASN.1 input file(s) or directory')
-    #parser.add_argument('-s', dest='spec', type=str,
-    #                    help='provide a specification shortname, instead of ASN.1 input file(s)')
+    parser.add_argument('-s', dest='spec', type=str,
+                        help='provide a specification shortname, instead of ASN.1 input file(s)')
     parser.add_argument('-o', dest='output', type=str, default='out',
                         help='compiled output Python (and json) source file(s)')
     parser.add_argument('-j', dest='json', action='store_true',
@@ -117,14 +121,45 @@ def main():
     else:
         ofd.close()
     
-    #if args.spec:
-    #    if args.spec not in ASN_SPECS:
-    #        print('%s, args error: invalid specification name %s' % (sys.argv[0], args.spec))
-    #        print_specnames()
-    #        return 0
-    #    compile_spec(shortname=args.spec, **ckw)
+    if args.spec:
+        if args.spec not in ASN_SPECS:
+            print('%s, args error: invalid specification name %s' % (sys.argv[0], args.spec))
+            print_specnames()
+            return 0
+        # get spec name and potential flags
+        specname = ASN_SPECS[args.spec]
+        if isinstance(specname, (tuple, list)):
+            for kw in specname[1:]:
+                if kw not in ckw:
+                    ckw[kw] = True
+            specname = specname[0]
+        specdir  = os.path.abspath(get_spec_dir(specname))
+        # compile the spec
+        GLOBAL.clear()
+        compile_spec(name=specname, **ckw)
+        # generate .txt files
+        objname = specdir + os.path.sep + 'load_obj.txt'
+        modname = specdir + os.path.sep + 'load_mod.txt'
+        if not os.path.exists(modname):
+            with open(modname, 'w') as fd:
+                for m in GLOBAL.MOD:
+                    if m[0] != '_':
+                        fd.write('%s.asn\n' % m)
+                print('%s file created' % modname)
+        if not os.path.exists(objname):
+            with open(objname, 'w') as fd:
+                for (m, n) in GLOBAL.COMP['DONE']:
+                    fd.write('%s.%s\n' % (m, n))
+                print('%s file created' % objname)
+        # generate python and json files
+        destname = os.path.abspath(specdir + os.path.sep + '..') + os.path.sep + args.spec
+        generate_modules(PycrateGenerator, destname + '.py')
+        print('%s file created' % (destname + '.py', ))
+        generate_modules(JSONDepGraphGenerator, destname + '.json')
+        print('%s file created' % (destname + '.json', ))
+        GLOBAL.clear()
     #
-    if args.input:
+    elif args.input:
         files = []
         for i in args.input:
             if os.path.isdir(i):
@@ -171,14 +206,14 @@ def main():
                 else:
                     fd.close()
         compile_text(txt, **ckw)
+        #
+        generate_modules(PycrateGenerator, args.output + '.py')
+        if args.json:
+            generate_modules(JSONDepGraphGenerator, args.output + '.json')
     #
     else:
         print('%s, args error: missing ASN.1 input(s) or specification name' % sys.argv[0])
-        return 0
-    
-    generate_modules(PycrateGenerator, args.output + '.py')
-    if args.json:
-        generate_modules(JSONDepGraphGenerator, args.output + '.json')
+    #
     return 0
 
 if __name__ == '__main__':
