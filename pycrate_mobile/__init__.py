3bdb89a14d56139998a717ec0e27179c94c7a231
p1-bmu <benoit.michau@p1sec.com>
2020-10-26 20:34:50 +0100

    mobile: initial support for all ISUP parameters

--- pycrate_mobile/__init__.py
+++ pycrate_mobile/__init__.py
@@ -28,12 +28,13 @@
 # *--------------------------------------------------------
 #*/
 #
-__all__ = ['GSMTAP', 'MCC_MNC', 'PPP', 'SIGTRAN', 'SCCP',
+__all__ = ['GSMTAP', 'MCC_MNC', 'PPP', 'SIGTRAN', 'SCCP', 'ISUP',
            'TS24007', 'NAS', 'NASLTE',
            'TS24008_IE', 'TS24008_MM', 'TS24008_GMM', 'TS24008_CC', 'TS24008_SM',
            'TS23038', 'TS23040_SMS', 'TS23041_CBS', 'TS24011_PPSMS', 'TS24080_SS',
            'TS24301_IE', 'TS24301_EMM', 'TS24301_ESM',
            'TS24501_IE', 'TS24501_FGMM', 'TS24501_FGSM', 'TS24501_UEPOL', 'TS24526_UEPOL',
+           'TS24588_UEPOL', 'TS24519_TSNAF',
            'TS29002_MAPAppCtx', 'TS29002_MAPIE', 'TS29274_GTPC', 'TS29281_GTPU',
            'TS44018_IE', 'TS44018_RR', 'TS44018_GTTP', 'TS48058_Abis']
 __version__ = '0.4.0'
