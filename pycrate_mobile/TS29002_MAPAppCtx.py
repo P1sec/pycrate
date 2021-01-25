30a792efdf9d446ba47ebccb0c38ba74df7efaf9
p1-bmu <benoit.michau@p1sec.com>
2021-01-25 16:30:05 +0100

    mobile: fix crappy license header in MAP files

--- pycrate_mobile/TS29002_MAPAppCtx.py
+++ pycrate_mobile/TS29002_MAPAppCtx.py
@@ -3,21 +3,22 @@
 # * Software Name : pycrate
 # * Version : 0.4.0
 # *
-# * Copyright © 2019. Benoit Michau. P1sec.
+# * Copyright 2019. Benoit Michau. P1sec.
 # *
-# * This program is free software: you can redistribute it and/or modify
-# * it under the terms of the GNU General Public License version 2 as published
-# * by the Free Software Foundation. 
+# * This library is free software; you can redistribute it and/or
+# * modify it under the terms of the GNU Lesser General Public
+# * License as published by the Free Software Foundation; either
+# * version 2.1 of the License, or (at your option) any later version.
 # *
-# * This program is distributed in the hope that it will be useful,
+# * This library is distributed in the hope that it will be useful,
 # * but WITHOUT ANY WARRANTY; without even the implied warranty of
-# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# * GNU General Public License for more details. 
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# * Lesser General Public License for more details.
 # *
-# * You will find a copy of the terms and conditions of the GNU General Public
-# * License version 2 in the "license.txt" file or
-# * see http://www.gnu.org/licenses/ or write to the Free Software Foundation,
-# * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
+# * You should have received a copy of the GNU Lesser General Public
+# * License along with this library; if not, write to the Free Software
+# * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+# * MA 02110-1301  USA
 # *
 # *--------------------------------------------------------
 # * File Name : pycrate_mobile/TS29002_MAPAppCtx.py
