66b01d52fb2127a9bc98bc2e274b4e1fcfc7e86d
p1-bmu <benoit.michau@p1sec.com>
2021-05-21 12:26:37 +0200

    asn1rt: force the presence of pyrcate_core, as it is in any way required by asn1rt/utils.py

--- pycrate_asn1rt/err.py
+++ pycrate_asn1rt/err.py
@@ -31,17 +31,12 @@
 # ASN.1 runtime errors
 #------------------------------------------------------------------------------#
 
-try:
-    from pycrate_core.utils import PycrateErr
-except:
-    print('pycrate_core not found, ASN1Err will not inherit from PycrateErr')
-    # generic ASN.1 error
-    class ASN1Err(Exception):
-        pass
-else:
-    # generic ASN.1 error
-    class ASN1Err(PycrateErr):
-        pass
+from pycrate_core.utils import PycrateErr
+
+
+# generic ASN.1 error
+class ASN1Err(PycrateErr):
+    pass
 
 # error when manipulating an existing ASN1 object
 class ASN1ObjErr(ASN1Err):
